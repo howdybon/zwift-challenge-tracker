6fc7a765bf982ba6c0c529253b0b41b932bcd710
paytongentry97 <78056311+paytongentry97@users.noreply.github.com>
2021-01-26 17:53:01 -0700

    commit

---
+++ Zwift_Challenge/Data/ZwiftChallengeService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Zwift_Challenge.Data
+{
+    public class ZwiftChallengeService
+    {
+        private static readonly string[] Contestants = new[]
+       {
+            "Payton", "Robby", "Parker"
+
+        };
+        public ZwiftChallengeService()
+        {
+        }
+        public async Task <string> GetWinner()
+        {
+            var random = new Random();
+            var winnerIndex = random.Next(0, 3);
+            var winner = Contestants[winnerIndex];
+            return winner;
+        }
+    }
+}
