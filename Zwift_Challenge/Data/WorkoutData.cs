794e33f2b9f22b0761e69cc717e3aae8a23aa948
Parker Burgess <parkerburgess@outlook.com>
2021-01-30 04:42:40 -0600

    added Monthly Records and Razden

---
+++ Zwift_Challenge/Data/WorkoutData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zwift_Challenge.Data
+{
+    public class WorkoutData
+    {
+        public DateTime Date { get; set; }
+        public string Rider { get; set; }
+        public TimeSpan Time { get; set; }
+        public float Distance { get; set; }
+        public int Elevation { get; set; }
+        public int Calories { get; set; }
+        public string Route { get; set; }
+    }
+}
--- Zwift_Challenge/Data/ZwiftChallengeService.cs
+++ Zwift_Challenge/Data/ZwiftChallengeService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Zwift_Challenge.Data
@@ -19,6 +20,54 @@ namespace Zwift_Challenge.Data
             var winnerIndex = random.Next(0, 3);
             var winner = Contestants[winnerIndex];
             return winner;
+        }
+
+        public async Task<IEnumerable<WorkoutData>> GetWorkoutsByMonth(string month)
+        {
+            var toReturn = GetMockData();
+            return toReturn;
+        }
+
+        private IEnumerable<WorkoutData> GetMockData()
+        {
+            var toReturn = new List<WorkoutData>();
+
+            var newWorkout = new WorkoutData
+            {
+                Calories = 1234,
+                Elevation = 234,
+                Time = new TimeSpan(2, 23, 23),
+                Date = new DateTime(2021, 2, 2),
+                Distance = (float)78.34,
+                Rider = "Parker",
+                Route = "Volcano"
+            };
+            toReturn.Add(newWorkout);
+
+            toReturn.Add( new WorkoutData
+            {
+                Calories = 123,
+                Elevation = 23,
+                Time = new TimeSpan(0, 23, 23),
+                Date = new DateTime(2021, 2, 2),
+                Distance = (float)7.34,
+                Rider = "Payton",
+                Route = "Volcano"
+            });
+
+            toReturn.Add(new WorkoutData
+            {
+                Calories = 1,
+                Elevation = 4,
+                Time = new TimeSpan(0, 3, 23),
+                Date = new DateTime(2021, 2, 2),
+                Distance = (float)0.34,
+                Rider = "Robby",
+                Route = "Volcano"
+            });
+
+            
+            return toReturn;
         }
     }
 }
